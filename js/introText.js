b4fe262429aa89d94d812df66ef2163d7bfbe9a2
MudaIsCarry <Rickambergen25@gmail.com>
2023-04-05 13:46:03 +0200

    jes

--- js/introText.js
+++ js/introText.js
@@ -11,6 +11,14 @@ function getMessage() {
   }
 
   window.onload = function() {
-    var greeting = document.getElementById("introText");
+    var greeting = document.getElementById("greeting");
     greeting.innerText = getMessage();
   };
+
+//   function getMessage() {
+
+//     window.onload = function() {
+//         var introText = document.getElementById("introText")
+//         introText.innerText = getMessage() + "Welkom op de website van Draak in Nood. Wij zijn een stichting die gewonde draakjes langs de weg helpt een nieuw thuis te zoeken.";
+//     }
+//   }
